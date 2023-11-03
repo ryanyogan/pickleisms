aaa0cbc1dcb9243fb501d9121958ef72a28ffd6e
Ryan Yogan <ryanyogan@gmail.com>
2023-11-02 23:18:47 -0500

    feat // lets checkout FL0

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use axum::routing::{get, Router};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let port = 3000;
+    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     let addr = format!("0.0.0.0:{}", port);
 
     let app = Router::new().route("/", get(health));
